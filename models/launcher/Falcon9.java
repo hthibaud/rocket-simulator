441713c19791fe4d4fd2660b3b826017843a8b98
hthibaud <thibaud.herry@ynov.com>
2026-04-28 01:24:59 +0200

    feat(Launcher): added the Falcon9 and SLS launcher classes

---
+++ models/launcher/Falcon9.java
@@ -0,0 +1,9 @@
+package models.launcher;
+
+public class Falcon9 extends Launcher{
+
+    public Falcon9(){
+        super("Falcon 9", 60, 500, 22, 0, true);
+    }
+    
+}
---
+++ models/launcher/SLS.java
@@ -0,0 +1,8 @@
+package models.launcher;
+
+public class SLS extends Launcher{
+    
+    public SLS(){
+        super("SLS", 2000, 2600, 130, 2, true);
+    }
+}

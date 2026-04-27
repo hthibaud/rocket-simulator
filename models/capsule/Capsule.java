c0293adc183e761ec2c62cf9503c2ff16a49e3a1
hthibaud <thibaud.herry@ynov.com>
2026-04-28 01:19:35 +0200

    feat(Booster): added the cargoDragon class for the capsules

--- models/capsule/Capsule.java
+++ models/capsule/Capsule.java
@@ -8,11 +8,23 @@ public abstract class Capsule {
     protected int price;
     protected double weight;
 
-    public Capsule(String name, boolean isManned, int maxPeople, int price, double weight){
+    public Capsule(String name, boolean isManned, int maxPeople, int price, double weight) {
         this.name = name;
         this.isManned = isManned;
         this.maxPeople = maxPeople;
         this.price = price;
         this.weight = weight;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+        public String getName(){
+        return name;
+    }
+    @Override
+    public String toString(){
+        return "Your capsule's name: " + getName();
     }
 }
---
+++ models/capsule/CargoDragon.java
@@ -0,0 +1,8 @@
+package models.capsule;
+
+public class CargoDragon extends Capsule{
+    
+    public CargoDragon(){
+        super("Cargo Dragon", false, 0, 100, 9.5);
+    }
+}

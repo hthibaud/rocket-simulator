88a9860976df2a1c3e1c0ff805540e36d80576aa
hthibaud <thibaud.herry@ynov.com>
2026-04-28 01:32:20 +0200

    feat(Booster): added the SRB and BE-3 boosters classes

---
+++ models/booster/BE3.java
@@ -0,0 +1,7 @@
+package models.booster;
+
+public class BE3 extends Booster{
+    public BE3(){
+        super("BE-3", 490, 25, 12);
+    }
+}
---
+++ models/booster/SRB.java
@@ -0,0 +1,7 @@
+package models.booster;
+
+public class SRB extends Booster{
+    public SRB(){
+        super("SRB", 12500, 590, 55);
+    }
+}

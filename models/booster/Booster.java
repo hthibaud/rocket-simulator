ec67af065485e07da5f5bb4472c9021d7588f968
hthibaud <thibaud.herry@ynov.com>
2026-04-28 01:19:06 +0200

    feat(Booster): did the booster EAP class

--- models/booster/Booster.java
+++ models/booster/Booster.java
@@ -1,16 +1,27 @@
 package models.booster;
 
 public class Booster {
-    
+
     protected String name;
     protected int additionalThrust;
     protected double weight;
     protected int price;
 
-    public Booster(String name, int additionalThrust, double weight, int price){
+    public Booster(String name, int additionalThrust, double weight, int price) {
         this.name = name;
         this.additionalThrust = additionalThrust;
         this.weight = weight;
         this.price = price;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+    public String getName(){
+        return name;
+    }
+    @Override
+    public String toString(){
+        return "Your booster: " + getName();
     }
 }
---
+++ models/booster/EAP.java
@@ -0,0 +1,7 @@
+package models.booster;
+
+public class EAP extends Booster{
+    public EAP(){
+        super("EAP Booster", 6470, 270, 30);
+    }
+}

bc1f0619fc04af3719dd4a2b474d1d8c859f74e3
hthibaud <thibaud.herry@ynov.com>
2026-04-28 17:24:36 +0200

    refactor(Launch): slightly modified this file

--- core/Launch.java
+++ core/Launch.java
@@ -5,7 +5,7 @@ import java.util.List;
 import models.mission.Mission;
 
 public class Launch {
-   
+
     private Rocket rocket;
     private Mission mission;
     private boolean success;
@@ -24,67 +24,63 @@ public class Launch {
         this.reason = reason;
     }
 
-    public double calculateMissionCost(Rocket myRocket, Mission myMission){
+    public double calculateMissionCost(Rocket myRocket, Mission myMission) {
         return myRocket.getTotalPrice() + (myRocket.getTotalFuel() * 1200);
     }
 
-    public String generateAndAddReport(){
+    public String generateAndAddReport() {
         String report = "Launch report of the " + date + ": ";
         reports.add(report);
         return report;
-    } 
+    }
 
-    public String isMissionSuccessful(Rocket myRocket, Mission myMission){
-        if (!verifyMissionCompatibility(myRocket, myMission)){
+    public String isMissionSuccessful(Rocket myRocket, Mission myMission) {
+        if (!verifyMissionCompatibility(myRocket, myMission)) {
             return "Your mission to" + myMission.getName() + "has been canceled.\n CAUSE: Your capsule had to be habitated.";
-        } else if (!verifyFuel(myRocket, myMission)){
+        } else if (!verifyFuel(myRocket, myMission)) {
             return "Your mission to" + myMission.getName() + "has been canceled.\n CAUSE: You had only" + myRocket.getTotalFuel() + "tons of fuel for " + myMission.calculateFuelNeeded(myRocket, myMission) + "tons of fuel needed.";
         } else if (!verifyTotalWeight(myRocket, myMission)) {
             return "Your mission to" + myMission.getName() + "has been canceled.\n CAUSE: Your capsule's weight is " + myRocket.getCapsule().getWeight() + "tons, but your rocket can only handle" + myRocket.getLauncher().getPayload() + "tons.";
         } else if (!verifyNbBoosters(myRocket)) {
             return "Your mission to" + myMission.getName() + "has been canceled.\n CAUSE: You had a maximum of " + myRocket.getLauncher().getMaxBoosters() + "boosters ot attach, but you attached" + myRocket.getNbBoosters(myRocket) + "boosters.";
-        }
-        else if (verifyMissionCompatibility(myRocket, myMission) && verifyFuel(myRocket, myMission) && verifyTotalWeight(myRocket, myMission) && verifyNbBoosters(myRocket)){
+        } else if (verifyMissionCompatibility(myRocket, myMission) && verifyFuel(myRocket, myMission) && verifyTotalWeight(myRocket, myMission) && verifyNbBoosters(myRocket)) {
             return "Your mission to" + myMission.getName() + "is a SUCCESS!";
-        }
-        else {
+        } else {
             return "Problem here.";
+        }
     }
-}
 
-    public boolean verifyFuel(Rocket myRocket, Mission myMission){
+    public boolean verifyFuel(Rocket myRocket, Mission myMission) {
         return (myMission.calculateFuelNeeded(myRocket, myMission) <= myRocket.getTotalFuel());
     }
 
-    public boolean verifyTotalWeight(Rocket myRocket, Mission myMission){
+    public boolean verifyTotalWeight(Rocket myRocket, Mission myMission) {
         return (myRocket.getCapsule().getWeight() <= myRocket.getLauncher().getPayload());
     }
 
-    public boolean verifyNbBoosters(Rocket myRocket){
+    public boolean verifyNbBoosters(Rocket myRocket) {
         return (myRocket.getNbBoosters(myRocket) <= myRocket.getMaxBoosters());
     }
 
-    public boolean verifyMissionCompatibility(Rocket myRocket, Mission myMission){
+    public boolean verifyMissionCompatibility(Rocket myRocket, Mission myMission) {
 
         if (myRocket.getCapsule().getIsManned() == false) {
 
-        switch (myMission.getName()
-
-        ) {
-            case "ISS" -> {
-                return false;
+            switch (myMission.getName()) {
+                case "ISS" -> {
+                    return false;
+                }
+                case "Moon" -> {
+                    return false;
+                }
+                case "Mars" -> {
+                    return false;
+                }
+                case "Orbit" -> {
+                    return true;
+                }
             }
-            case "Moon" -> {
-                return false;
-            }
-            case "Mars" -> {
-                return false;
-            }
-            case "Orbit" -> {
-                return true;
-            }
-        } 
-    }
+        }
         return true;
     }
 }

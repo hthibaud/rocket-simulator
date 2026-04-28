c66e4bdf5d16b8c14a7acc9b8d63be0536cebb16
hthibaud <thibaud.herry@ynov.com>
2026-04-28 02:06:57 +0200

    refactor(utils): modified the clearConsole method

--- utils/utils.java
+++ utils/utils.java
@@ -3,7 +3,8 @@ package utils;
 public class utils {
 
     public static void clearConsole() {
-        System.out.print("\033[H\033[2J");
-        System.out.flush();
+
+        for (int i = 0; i < 50; ++i) System.out.println();
+        
     }
 }

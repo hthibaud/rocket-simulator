257f37fe90e7feb18764018cd22270e660635243
hthibaud <thibaud.herry@ynov.com>
2026-04-28 17:24:51 +0200

    feat(view): add ASCII art representation of rocket and integrate into start menu

--- view/Menu.java
+++
@@ -1,8 +0,0 @@
-package view;
-
-public class Menu {
-    
-    public static void mainMenu(){
-        System.out.println("################# WELCOME TO YOUR ROCKET SIMULATOR #################");
-    }
-}
